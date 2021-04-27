33863045732fef03c4addbd28b2a158fe1b39372
用户名 <1594557250@qq.com>
2021-04-27 16:46:07 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+
+import { FieldConfig } from './dynamic-form/models/field-config.interface';
+import { DynamicFormComponent } from './dynamic-form/containers/dynamic-form/dynamic-form.component';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['app.component.scss'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-forms';
+export class AppComponent implements AfterViewInit,OnInit {
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
+  ngOnInit(): void {
+    setTimeout(()=>{
+      this.form.setDisabled('submit', true);
+      this.form.setValue('name', 'Todd Motto');      
+    })
+  }
+
+  listData=[
+    {
+      type: 'input',
+      label: 'Full name',
+      name: 'name',
+      placeholder: 'Enter your name',
+      validation: [Validators.required, Validators.minLength(4)]
+    },
+    {
+      type: 'select',
+      label: 'Favourite Food',
+      name: 'food',
+      options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
+      placeholder: 'Select an option',
+      validation: [Validators.required]
+    },
+    {
+      label: 'Submit',
+      name: 'submit',
+      type: 'button'
+    }
+  ]
+  config: FieldConfig[] = this.listData
+
+  ngAfterViewInit() {
+    let previousValid = this.form.valid;
+    this.form.changes.subscribe(() => {
+      if (this.form.valid !== previousValid) {
+        previousValid = this.form.valid;
+        this.form.setDisabled('submit', !previousValid);
+      }
+    });
+  }
+
+  submit(value: {[name: string]: any}) {
+    // console.log(this.form)
+    console.log(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DynamicFormModule } from './dynamic-form/dynamic-form.module';
+// import { NzGridModule } from 'ng-zorro-antd/grid';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    DynamicFormModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ],
+  declarations: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
